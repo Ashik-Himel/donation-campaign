a870870d39407e84535051f0d6f4b93f00c6f393
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-09-25 13:18:14 +0600

    donation page's see all functionality added

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -7,6 +7,7 @@ const Donation = () => {
   const donatedIDs = getStorageItems();
   const allData = useContext(AllData);
   const [matchedIDs, setMatchedIDs] = useState([]);
+  const [isAll, setIsAll] = useState(false);
   useEffect( () => {
     if (allData.length > 0) {
       const matched = allData.filter(data => donatedIDs.includes(data.id));
@@ -20,9 +21,14 @@ const Donation = () => {
         <div className="container">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
             {
-              matchedIDs.map(data => <DonationCard key={data.id} data={data}></DonationCard>)
+              isAll ? matchedIDs.map(data => <DonationCard key={data.id} data={data}></DonationCard>) : matchedIDs.slice(0, 4).map(data => <DonationCard key={data.id} data={data}></DonationCard>)
             }
           </div>
+          {
+            !isAll && <div className="mt-8 text-center">
+              <button className="btn bg-[#009444] text-white hover:bg-[#009444] border border-[#009444] hover:border-[#009444] normal-case text-base" onClick={() => setIsAll(!isAll)}>See All</button>
+            </div>
+          }
         </div>
       </section>
     </main>
