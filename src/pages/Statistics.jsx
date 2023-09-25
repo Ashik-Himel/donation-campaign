8ff766aba58fff6953e79b7c0b8cce34e55af556
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-09-25 14:25:25 +0600

    pie chart added

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,7 +1,32 @@
+import { Chart } from "react-google-charts";
+import { getStorageItems } from "../localStorage";
+import { useEffect, useState } from "react";
+
+
+
 const Statistics = () => {
+  const [donated, setDonated] = useState(0);
+  let data = [
+    ["Donation", "Percentage"],
+    ["Total Donation", 12 - donated],
+    ["Your Donation", donated]
+  ];
+  useEffect( () => {
+    const donatedPercentage = getStorageItems().length;
+    setDonated(donatedPercentage);
+  }, []);
+
   return (
     <main>
-      <h1>Statistics</h1>
+      <section>
+        <div className="container">
+        <Chart
+          chartType="PieChart"
+          data={data}
+          width={"100%"}
+        />
+        </div>
+      </section>
     </main>
   );
 };
