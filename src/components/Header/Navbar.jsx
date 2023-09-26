9e5471c716fb80627ec763a89999abf68dd7227f
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-09-26 18:35:37 +0600

    hamburger menu overlaping fixed

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <Link to={'/'}>
         <img src="/images/logo.png" alt="Logo" className="max-w-[180px]" />
       </Link>
-      <ul className="flex flex-col sm:flex-row gap-4 sm:gap-6 absolute sm:static top-[80px] right-0 bg-white sm:[background-color:transparent] p-6 sm:p-0 border sm:border-none border-gray-300 rounded-lg sm:rounded-none shadow-lg sm:shadow-none overflow-hidden sm:overflow-auto h-[155px] sm:!h-auto transition-all sm:transition-none" style={navHidden ? {height: '0', paddingBlock: '0', borderWidth: '0'} : null}>
+      <ul className="flex flex-col sm:flex-row gap-4 sm:gap-6 absolute sm:static top-[80px] right-0 bg-white sm:[background-color:transparent] p-6 sm:p-0 border sm:border-none border-gray-300 rounded-lg sm:rounded-none shadow-lg sm:shadow-none overflow-hidden sm:overflow-auto h-[155px] sm:!h-auto transition-all sm:transition-none z-10" style={navHidden ? {height: '0', paddingBlock: '0', borderWidth: '0'} : null}>
         <li>
           <NavLink to={'/'} className={({isActive}) => isActive ? 'text-[#FF444A] font-bold border-b-2 border-[#FF444A]' : ''} onClick={() => setNavHidden(!navHidden)}>Home</NavLink>
         </li>
